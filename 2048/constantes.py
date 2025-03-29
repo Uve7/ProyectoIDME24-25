60797349966231fe56df4d9fc241f7fcad1804e8
Uve7 <loskikos7@gmail.com>
2025-03-29 22:57:08 +0100

    Creación de celdas, creacion de colores y prueba de color en la pantalla

--- 2048/constantes.py
+++ 2048/constantes.py
@@ -1,4 +1,29 @@
-from os import system 
-#Declaración de las dimensiones de la pantala
+from os import system  
+
+#declaración de las dimensiones de la pantala
 ScreenWidth = 400
 ScreenHeight = 400
+
+#declaración de las dimensiones de cada celda
+CellSize = ScreenWidth/4
+
+#colores del juego
+blanco  = (255,255,255)
+negro = (0,0,0)
+rojo = (255,0,0)
+
+#diccionario con loscolores de los numeros
+colors = {
+    0: (204,192,179),
+    2: (238,228,218),
+    4: (237,224,179),
+    8: (242,177,121),
+    16: (245,149,99),
+    32: (246,124,95),
+    64: (246,94,59),
+    128: (237,207,114),
+    256: (237,204,97),
+    512: (237,200,80),
+    1024: (237,197,63),
+    2048: (237,194,46),
+}
--- 2048/main.py
+++ 2048/main.py
@@ -1,8 +1,10 @@
+#importacion de librerias
 from os import system 
-import pygame #importacion de la libreria pygame
+import pygame 
 import constantes
+import sys
 
-
+print(dir(constantes))
 print(dir(constantes))
 #inicializacion de Pygame
 pygame.init()
@@ -13,11 +15,25 @@ screen = pygame.display.set_mode((constantes.ScreenWidth, constantes.ScreenHeigh
 #titulo del juego
 pygame.display.set_caption("2048")
 
+#creacion de matriz para el tablero
+tablero = [[0] * 4 for _ in range(4)] #crea los elementos numéricos a 0 para que no se muestren
+
+#variable que controla el estado del juego
+gameOver = False
+gameWon = False
+
+#funciones necesarias para el juego
+def dibujaPantalla (): #pone en blanca la pantalla
+    screen.fill(constantes.blanco)
+
 #controla la pantalla segun los eventos que ocurran
 running = True 
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+        if event.type == pygame.QUIT: #evento que cierra la pantalla al pulsar la X
+            pygame.quit() 
+    dibujaPantalla()
+    pygame.display.flip()
+
            
     
