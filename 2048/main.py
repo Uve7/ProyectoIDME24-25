38e2e12997bc5497ba605acf885d234a2094bcc5
Uve7 <loskikos7@gmail.com>
2025-03-31 11:29:34 +0200

    Se avanza en la funcion dibujaPantalla(). Añadido texto en celdas y coordenadas especificas y mensaje de victoria/derrota.

--- 2048/main.py
+++ 2048/main.py
@@ -4,8 +4,6 @@ import pygame
 import constantes
 import sys
 
-print(dir(constantes))
-print(dir(constantes))
 #inicializacion de Pygame
 pygame.init()
 
@@ -23,17 +21,54 @@ gameOver = False
 gameWon = False
 
 #funciones necesarias para el juego
-def dibujaPantalla (): #pone en blanca la pantalla
-    screen.fill(constantes.blanco)
+def dibujaPantalla (): #controla parámetros de la pantalla
+    screen.fill(constantes.blanco) #llama a la constante blanco para poner la pantalla en blanco
+    
+    font = pygame.font.Font(None,40) #creación de una fuente específica
+    fuenteMensaje = pygame.font.Font(None, 30) #creción de una fuente para los mensajes
 
+    for i in range(4): #control del valor de cada celda
+        for j in range(4):
+            value = tablero [i][j]
+            color = constantes.colors.get(value, constantes.colors[0]) #pone cad avalor en el color especificado en constantes
+            
+            #dibuja cada celda, rect dice que es un rectángulo. Coge las coordenadas y el color especificado
+            pygame.draw.rect(screen, color,
+                             (j * constantes.CellSize, #coordenada 
+                              i * constantes.CellSize, #coordenada
+                                  constantes.CellSize, 
+                                  constantes.CellSize, #widht:0
+                              ))
+            if value != 0: #hay que dibujar el numero cuando sea distinto de 0
+                text = font.render(str(value), True, constantes.negro) #convertimos el valor a string, el true es para suavizar y cogemos la cosntante negro
+                
+                #pone el texto en el centro de la celda
+                text_rect = text.get_rect(j * constantes.CellSize + constantes.CellSize //2, 
+                                           i* constantes.CellSize + constantes.CellSize //2) 
+                #blit dibuja una imagen dentro de otra, el texto text en la coordenada text_rect
+                screen.blit(text,text_rect) 
+
+            #diferentes mensajes depdneindo de si gaans o pierdes                    
+            if gameWon:
+                text = fuenteMensaje.render("¡Enhorabuena! Has ganado, presiona R para reiniciar", True, constantes.rojo) #mensaje, suavizado, color
+                screen.blit(text, (50, constantes.ScreenHeight // 2-20)) #posicion del texto
+            elif gameOver:
+                text = fuenteMensaje.render("¡Eres malísima¡ Has perdido, presiona R para reinicar.", True, constantes.rojo)
+                screen.blit(text, (50, constantes.ScreenHeight // 2-20))
+
+              
 #controla la pantalla segun los eventos que ocurran
 running = True 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #evento que cierra la pantalla al pulsar la X
             pygame.quit() 
-    dibujaPantalla()
-    pygame.display.flip()
+            sys.exit()
+    dibujaPantalla() #llama a la funcion
+    pygame.display.flip() #actualiza la pantalla
+
+
+
 
            
     
